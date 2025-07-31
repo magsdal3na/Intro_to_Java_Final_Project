49c4f256ab012e3fe1d810cd1e75408ee23e18e1
Maggie Voigts <142745784+magsdal3na@users.noreply.github.com>
2025-07-31 16:39:14 -0500

    Update Hangman.java
    Added spaces functionality to track which letters are correct. Also added a temporary game loop of 12 iterations to help test more letters.
    
    Need to add functionality for ending the game if all spaces have been replaced next.

--- Hangman.java
+++ Hangman.java
@@ -1,6 +1,7 @@
 import java.util.Scanner; // needed to read player input
 import java.util.Random; // needed to generate random word
 import java.util.ArrayList; // needed to hold word list
+import java.util.Arrays; // needed to print out and use spaces
 
 public class Hangman
 {
@@ -21,6 +22,8 @@ public class Hangman
 		int randomIndex = randomGenerator.nextInt(words.size());
 		// Holds random word
 		String randomWord = words.get(randomIndex);
+		// Initialize spaces for letters
+		String[] spaces = {"-","-","-","-","-","-"};
 		// Initializes menu screen and starting image
 		System.out.println("Welcome to Hangman!");
 		System.out.println("  -----");
@@ -30,7 +33,7 @@ public class Hangman
 		System.out.println("  |  / \\");
 		System.out.println("  |  ");
 		System.out.println("-----");
-    
+
 		//do...while loop to handle selection choice
 		do {
 			System.out.println("Choose an option: ");
@@ -57,27 +60,41 @@ public class Hangman
 			}
 		} while (true);
 
-		// Start of game
-		System.out.println("  -----");
-		System.out.println("  |   |");
-		System.out.println("  |    ");
-		System.out.println("  |    ");
-		System.out.println("  |    ");
-		System.out.println("  |    ");
-		System.out.println("-----  ");
-		System.out.println("Choose your first letter: ");
+		for(int g = 0; g < 12; g++) {
+			// Start of game
+			System.out.println("  -----");
+			System.out.println("  |   |");
+			System.out.println("  |    ");
+			System.out.println("  |    ");
+			System.out.println("  |    ");
+			System.out.println("  |    ");
+			System.out.println("-----  ");
+			System.out.println("Choose a letter: ");
 
-		// Choosing letter
-		char inputChar = input.next().charAt(0);
-		System.out.println("You chose: " + inputChar);
-		input.nextLine();
-		// Checking for letter occurence 
-		if(randomWord.toLowerCase().contains(String.valueOf(inputChar).toLowerCase())) {
-		    System.out.println("That letter is in the word.");
-		}
-		else {
-		    System.out.println("That letter is incorrect.");
-		}
+			// Choosing letter
+			char inputChar = input.next().charAt(0);
+			System.out.println("You chose: " + inputChar);
+			input.nextLine();
+			char lowerCaseInputChar = Character.toLowerCase(inputChar);
+			// Checking for letter occurence
+			if(randomWord.toLowerCase().contains(String.valueOf(inputChar).toLowerCase())) {
+				System.out.println("That letter is in the word.");
+				// Identifying where the letter is at in the word and
+				// putting the letter in the spaces array if identified
+				for (int i = 0; i < randomWord.length(); i++) {
+					char wordChar = randomWord.charAt(i);
+					if (Character.toLowerCase(wordChar) == lowerCaseInputChar) {
+						spaces[i] = String.valueOf(inputChar);
+						System.out.println(Arrays.toString(spaces));
+					}
+				}
+			}
+			else {
+				System.out.println("That letter is not in the word.");
+			}
+		} // closes for loop running the game 
+		// Testing line to see what the word was & test spaces
 		System.out.println(randomWord);
+		System.out.println(Arrays.toString(spaces));
 	}
 }
